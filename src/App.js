772f9b4752c00b3ac37a330cd44b179c00311a87
Bablu Kumar <bkss1990@gmail.com>
2018-01-11 07:19:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Layout from "./components/Layout/Layout"
+class App extends Component {
+  render() {
+    return (
+
+
+      <div >
+      <Layout>
+
+        <p>
+
+          Hello I am from test 
+        </p>
+      </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
